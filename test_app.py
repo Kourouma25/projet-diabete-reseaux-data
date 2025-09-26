db7310d37fb4e10d61b895d876fbf6bbd3e23a4d
kourouma <kourouma92sekouba@gmail.com>
2025-09-26 21:33:04 +0200

    test unitaire / test de modèle, il teste la validité du modèle enregistré en s’assurant que la probabilité prédite pour une observation de test est bien comprise entre 0 et 1.

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import requests
+import pytest
+import joblib
+
+# Charger les données de test
+data = pd.read_csv('patient_a_tester.csv')
+donne_predire = data.iloc[0: 1,:]
+# Charger le modèle
+model_enregistre = joblib.load('random_forest_model.pkl')
+
+
+def test_predire():
+
+    score = model_enregistre.predict_proba(donne_predire)[0]
+    score = score[0]
+
+    assert score >= 0 
+    assert score <= 1
+    """Test pour vérifier la route de prédiction"""
